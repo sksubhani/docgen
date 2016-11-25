8594c3b8ef64d2e776553d48b1571728f766fe31
sksubhani <sk_subha@yahoo.com>
2016-11-25 11:46:56 -0800

    Initial commit

---
+++ src/main/java/com/subhani/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.subhani;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World, from Subhani - SVN personal repo code on Heroku");
+
+	}
+
+}
